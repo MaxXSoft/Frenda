8424da5cc62164562b9a1972bae8c97fa6c3cdcf
MaxXSoft <x@MaxXSoft.net>
2021-11-18 18:36:10 +0800

    (FrendaStage, FirrtlCompiler) added class 'FirrtlCompiler'

--- src/main/scala/frenda/FirrtlCompiler.scala
+++ src/main/scala/frenda/FirrtlCompiler.scala
@@ -1,6 +1,26 @@
 package frenda
 
+import firrtl.AnnotationSeq
 import firrtl.options.StageMain
 import frenda.stage.FrendaStage
 
+/**
+ * FIRRTL compiler of Frenda.
+ *
+ * @param annotations the input annotation sequence from command line
+ */
+private class FirrtlCompiler(override val annotations: AnnotationSeq)
+  extends HasFrendaOptions {
+
+  /**
+   * Compiles the input FIRRTL incrementally.
+   */
+  def compile(): Unit = {
+    // TODO
+  }
+}
+
+/**
+ * Entry point of Frenda.
+ */
 object FirrtlCompiler extends StageMain(new FrendaStage)
--- src/main/scala/frenda/stage/FrendaStage.scala
+++ src/main/scala/frenda/stage/FrendaStage.scala
@@ -3,12 +3,18 @@ package frenda.stage
 import firrtl.AnnotationSeq
 import firrtl.options.{Shell, Stage}
 import firrtl.stage.FirrtlCli
+import frenda.FirrtlCompiler
 
+/**
+ * Main stage of Frenda.
+ *
+ * The stage will read the input FIRRTL, and then compile it incrementally.
+ */
 class FrendaStage extends Stage {
   val shell: Shell = new Shell(applicationName = "frenda") with FrendaCli with FirrtlCli
 
   def run(annotations: AnnotationSeq): AnnotationSeq = {
-    // TODO
+    new FirrtlCompiler(annotations).compile()
     annotations
   }
 }
