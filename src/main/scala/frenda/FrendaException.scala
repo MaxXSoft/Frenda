9b87063b4d2c60f32a2a2375e236582f2bb11e8f
MaxXSoft <x@MaxXSoft.net>
2021-11-19 15:42:26 +0800

    (frenda) modified some details about comments

--- src/main/scala/frenda/FrendaException.scala
+++ src/main/scala/frenda/FrendaException.scala
@@ -2,6 +2,7 @@ package frenda
 
 /**
  * Represents a Frenda internal exception.
+ *
  * @param msg message of the exception
  */
 class FrendaException(msg: String) extends Exception(msg)
--- src/main/scala/frenda/stage/FrendaCli.scala
+++ src/main/scala/frenda/stage/FrendaCli.scala
@@ -2,6 +2,9 @@ package frenda.stage
 
 import firrtl.options.Shell
 
+/**
+ * The command line interface of Frenda.
+ */
 trait FrendaCli {
   this: Shell =>
   parser.note("Frenda Specific Options")
