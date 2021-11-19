83e64402cf6dade8e9840242467b9a1f0446dc5d
MaxXSoft <x@MaxXSoft.net>
2021-11-19 16:29:39 +0800

    (FirrtlCompiler) removed some redundant code

--- src/main/scala/frenda/FirrtlCompiler.scala
+++ src/main/scala/frenda/FirrtlCompiler.scala
@@ -1,24 +1,7 @@
 package frenda
 
-import firrtl.AnnotationSeq
 import firrtl.options.StageMain
 import frenda.stage.FrendaStage
-
-/**
- * FIRRTL compiler of Frenda.
- *
- * @param annotations the input annotation sequence from command line
- */
-private class FirrtlCompiler(override val annotations: AnnotationSeq)
-  extends HasFrendaOptions {
-
-  /**
-   * Compiles the input FIRRTL incrementally.
-   */
-  def compile(): Unit = {
-    // TODO
-  }
-}
 
 /**
  * Entry point of Frenda.
