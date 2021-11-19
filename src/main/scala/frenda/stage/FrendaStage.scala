b993a83c8d8ad0451748247ace1a1a9a2a7d2c54
MaxXSoft <x@MaxXSoft.net>
2021-11-19 15:08:25 +0800

    (FrendaStage) modified some details about phase

--- src/main/scala/frenda/stage/FrendaStage.scala
+++ src/main/scala/frenda/stage/FrendaStage.scala
@@ -3,7 +3,6 @@ package frenda.stage
 import firrtl.AnnotationSeq
 import firrtl.options.{Shell, Stage}
 import firrtl.stage.FirrtlCli
-import frenda.FirrtlCompiler
 
 /**
  * Main stage of Frenda.
@@ -13,8 +12,5 @@ import frenda.FirrtlCompiler
 class FrendaStage extends Stage {
   val shell: Shell = new Shell(applicationName = "frenda") with FrendaCli with FirrtlCli
 
-  def run(annotations: AnnotationSeq): AnnotationSeq = {
-    new FirrtlCompiler(annotations).compile()
-    annotations
-  }
+  def run(annotations: AnnotationSeq): AnnotationSeq = new FrendaPhase().transform(annotations)
 }
