2d3be58207ee55b7a3a1a54445779cadc8af4f48
MaxXSoft <x@MaxXSoft.net>
2021-11-20 16:07:51 +0800

    (AddCircuit) added some logs

--- src/main/scala/frenda/stage/phases/AddCircuit.scala
+++ src/main/scala/frenda/stage/phases/AddCircuit.scala
@@ -4,6 +4,7 @@ import firrtl.AnnotationSeq
 import firrtl.options.{Dependency, Phase}
 import firrtl.stage.phases
 import firrtl.stage.phases.AddDefaults
+import frenda.stage.FrendaOptions
 
 class AddCircuit extends Phase {
   override def prerequisites = Seq(Dependency[CheckOptions], Dependency[AddDefaults])
@@ -14,6 +15,9 @@ class AddCircuit extends Phase {
 
   override def invalidates(a: Phase) = false
 
-  override def transform(annotations: AnnotationSeq): AnnotationSeq =
+  override def transform(annotations: AnnotationSeq): AnnotationSeq = {
+    val options = FrendaOptions.fromAnnotations(annotations)
+    options.log("Parsing input circuit...")
     new phases.AddCircuit().transform(annotations)
+  }
 }
